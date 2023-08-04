9f6546b6e17f3c400ad0403e372a5095d9105f67
Gorkem Agac <gorkem_agac@hotmail.com>
2023-08-05 00:09:05 +0100

    TSET Sample Framework

---
+++ src/test/java/step_definitions/Hooks.java
@@ -0,0 +1,32 @@
+package step_definitions;
+
+import io.cucumber.java.After;
+import io.cucumber.java.Before;
+import io.cucumber.java.Scenario;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import pages.BasePage;
+import utilities.ConfigurationReader;
+import utilities.Driver;
+
+import java.util.concurrent.TimeUnit;
+
+public class Hooks {
+
+    BasePage basePage = new BasePage();
+
+    @Before
+    public void setup() {
+        Driver.getDriver().get(ConfigurationReader.getProperty("web_url"));
+        Driver.getDriver().manage().window().maximize();
+        Driver.getDriver().manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+    }
+    @After
+    public void tearDown(Scenario scenario){
+        if (scenario.isFailed()){
+            byte [] screenshot = ((TakesScreenshot) Driver.getDriver()).getScreenshotAs(OutputType.BYTES);
+            scenario.attach(screenshot, "image/png", scenario.getName());
+        }
+        Driver.closeDriver();
+    }
+}
